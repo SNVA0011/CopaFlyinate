a287a07b48b350391efe2a6c6029fd207ebfd7af
SNVA0011 <96859907+SNVA0011@users.noreply.github.com>
2023-12-12 14:53:49 +0530

    airlines-pages-ui

--- pages/_document.js
+++ pages/_document.js
@@ -8,15 +8,13 @@ class MyDocument extends Document {
       <Html lang="es">
         <Head>
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;700&family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
-
           <meta name="google-site-verification" content="vqR1JrGbf_OrfNp94N1r_TRIT1BKtKucQc3fr3EagG0" />
- 
         </Head>
-        <body> 
+        <body>
 
           <Main />
           <NextScript />
